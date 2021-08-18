7c2de90dab2093cf1bc22969b85017745bb1fac9
Keller McBride <keller.mcbride@gmail.com>
2021-08-18 10:07:55 -0500

    Adding Class zoommeeting - not used yet.

--- main.py
+++ main.py
@@ -6,13 +6,100 @@ from time import time
 import csv
 from datetime import datetime
 
-#Globals
+# Globals
 bearer = ""
 API_KEY = ""
 API_SEC = ""
 inputFile = ""
 outputFileJSON = ""
 outputFileCSV = ""
+
+
+class zoomMeeting:
+
+    def __init__(self, type, topic, start_time, duration, timezone):
+        self.topic = topic
+        self.type = type
+        self.pre_schedule = False
+        if type == 2:
+            self.pre_schedule = True
+        self.start_time = start_time
+        self.duration = duration
+        self.schedule_for = ""
+        self.timezone = timezone
+        self.password = ""
+        self.agenda = ""
+        self.tracking_fields = []
+
+
+"""
+                recurrence{
+                    type,
+                    repeat_interval,
+                    weekly_days,
+                    monthly_day,
+                    monthly_week,
+                    monthly_week_day,
+                    end_times,
+                    end_date_time
+                     },
+                settings{
+                    host_video,
+                    participant_video,
+                    cn_meeting,
+                    in_meeting,
+                    join_before_host,
+                    jbh_time,
+                    mute_upon_entry,
+                    watermark,
+                    use_pmi,
+                    approval_type,
+                    registration_type,
+                    audio,
+                    alternative_hosts,
+                    close_registration,
+                    waiting_room,
+                    global_dial_in_countries[],
+                    contact_name,
+                    contact_email,
+                    registrants_email_notification,
+                    registrants_confirmation_email,
+                    meeting_authentication,
+                    authentication_option,
+                    authentication_domains,
+                    authentication_exception[{
+                        name,
+                        email
+                    }],
+                    additional_data_center_regions[],
+                    breakout_room{
+                        enable,
+                        rooms[{
+                            name,
+                            participants[]
+                        }],
+                        language_interpretation{
+                            enable,
+                            interpreters[{
+                                email,
+                                languages
+                                }]
+                            },
+                        show_share_button,
+                        allow_multiple_devices,
+                        encryption_type,
+                        approved_or_denied_countries_or_regions{
+                            enable,
+                            method,
+                            approved_list[],
+                            denied_list[],
+                            },
+                        alternative_hosts_email_notification
+                    },
+                    template_id
+                     }
+                )
+"""
 
 """
 Load the keys from config.json file
@@ -26,8 +113,8 @@ The config.json file looks like this:
   "outputFileCSV": "CSV path/filename with combined saba zoom info"
 }
 
-NOTE - if you are using your JWT bearer Token, you should leave API_KEY and API_SEC as ""
-
+NOTE - if you are using your JWT bearer Token, you should leave
+        API_KEY and API_SEC as ''
 """
 # Read in the global variables from config.json
 try:
@@ -43,6 +130,8 @@ except:
     print("An exception occured reading in the config.json file.")
 
 # Generate a token using the pyjwt library
+
+
 def generateToken():
     token = jwt.encode(
         # Create a payload of the token containing API Key & expiration time
@@ -56,6 +145,7 @@ def generateToken():
     return token
     # send a request with headers including a token
 
+
 """ Read in list of meetings from saba file
     Create Array of meetings to be created on zoom
 
@@ -63,8 +153,10 @@ Input file is determined by "inputFile" field in ./config.json file.
 The input file must be in CSV format.
 
 """
+
+
 def getCSV():
-    print("\nReading CSV data from file " + (inputFile) )
+    print("\nReading CSV data from file " + (inputFile))
     meetingDetails = []
 
     with open(inputFile) as csvfile:
@@ -73,103 +165,122 @@ def getCSV():
             startDate = rows["startDate"]
             classStartTime = rows["startTime"]
             startTimeSplit = classStartTime.split()
-            meetingStartDateTime = (startDate + ' ' + startTimeSplit[0] + ' ' + startTimeSplit[1])
-            meetingStart = datetime.strptime(meetingStartDateTime, "%d-%b-%Y %I:%M:%S %p")
+            meetingStartDateTime = (
+                startDate + ' ' + startTimeSplit[0] + ' ' + startTimeSplit[1])
+            meetingStart = datetime.strptime(
+                meetingStartDateTime, "%d-%b-%Y %I:%M:%S %p")
             startTime = meetingStart.strftime("%Y-%m-%dT%H:%M:%S")
             classLocation = rows["location"]
             locationSplit = classLocation.split()
             timezone = ""
             if locationSplit[1] == "Eastern":
-                    timezone = "America/New_York"
+                timezone = "America/New_York"
             if locationSplit[1] == "Central":
-                    timezone = "America/Chicago"
+                timezone = "America/Chicago"
             if locationSplit[1] == "Mountain":
-                    timezone = "America/Denver"
-            if locationSplit[1] =="Pacific":
-                    timezone = "America/Los_Angeles"
+                timezone = "America/Denver"
+            if locationSplit[1] == "Pacific":
+                timezone = "America/Los_Angeles"
 
             meetingDetails.append(
                 {
-                "tracking_fields": [
+                    "tracking_fields": [
                                     {"field": "CLASS_ID",
-                                    "value": rows["CLASS_ID"]
-                                    }],
-                "Saba_ID": rows["CLASS_ID"],
-                "topic": rows["topic"],
-                "type": 2,
-                "host": rows["host"],
-                "start_time": (startTime),
-                "agenda": "",
-                "schedule_for": "",
-                "recurrence": {"type": 1,
-                             "repeat_interval": 1
-                             },
-                "timezone":(timezone),
-                "duration":rows["duration"],
-                "settings": {"host_video": "true",
-                             "participant_video": "true",
-                             "join_before_host": "False",
-                             "jbh_time": 5,
-                             "mute_upon_entry": "False",
-                             "watermark": "true",
-                             "audio": "both",
-                             "auto_recording": "none",
-                             "waiting_room": "True",
-                             "alternative_hosts": rows["alternative_host"],
-                             "alternative_hosts_email_notification": "False"
-                             }
+                                     "value": rows["CLASS_ID"]
+                                     }],
+                    "Saba_ID": rows["CLASS_ID"],
+                    "topic": rows["topic"],
+                    "type": 2,
+                    "host": rows["host"],
+                    "start_time": (startTime),
+                    "agenda": "",
+                    "schedule_for": "",
+                    "recurrence": {"type": 1,
+                                   "repeat_interval": 1
+                                   },
+                    "timezone": (timezone),
+                    "duration": rows["duration"],
+                    "settings": {"host_video": "true",
+                                 "participant_video": "true",
+                                 "join_before_host": "False",
+                                 "jbh_time": 5,
+                                 "mute_upon_entry": "False",
+                                 "watermark": "true",
+                                 "audio": "both",
+                                 "auto_recording": "none",
+                                 "waiting_room": "True",
+                                 "alternative_hosts":
+                                 rows["alternative_host"],
+                                 "alternative_hosts_email_notification":
+                                 "False"
+                                 }
                 })
 
-        #print(meetingDetails)
+        # print(meetingDetails)
         return(meetingDetails)
 
 # Check to be sure all the hosts are zoom registered with paid license
+
+
 def checkLicense(meetingsList):
     if bearer != "":
         headers = {'authorization': ('Bearer ' + (bearer))}
     else:
         headers = {'authorization': 'Bearer %s' % generateToken()}
-    print("\nChecking each instructor and alternate to see if they are a Licensed Zoom Host: \n")
-    #Check Each Host
+    print("\nChecking each instructor and alternate to see if they are a "
+          + "Licensed Zoom Host: \n")
+    # Check Each Host
     for eachMeeting in meetingsList:
         zoomUser = eachMeeting['host']
         url = "https://api.zoom.us/v2/users/" + str(zoomUser)
         payload = {}
         hostStatus = "True"
-        try :
-            response = requests.request("GET", url, headers=headers, data=payload)
-            data=json.loads(response.text)
-            if data["type"] != 2 :
-                print("\n*** " + (zoomUser) + " is a free user. " + "Class_No " + eachMeeting["Saba_ID"] + "\n")
+        try:
+            response = requests.request(
+                "GET", url, headers=headers, data=payload)
+            data = json.loads(response.text)
+            if data["type"] != 2:
+                print("\n*** " + (zoomUser) + " is a free user. "
+                      + "Class_No " + eachMeeting["Saba_ID"] + "\n")
                 hostStatus = "False"
-            else :
-                print('.', end='')
-        except  :
-            print("\n*** " + (zoomUser) + " is not in this account's Zoom user list. " + "Class_No " + eachMeeting["Saba_ID"])
+            else:
+                print('.')
+        except:
+            print("\n*** " + (zoomUser) + " is not in this account's Zoom "
+                  + "user list. "
+                  + "Class_No "
+                  + eachMeeting["Saba_ID"])
             hostStatus = "False"
-    #Check each Alternate host
+    # Check each Alternate host
     for eachMeeting in meetingsList:
         zoomUser = eachMeeting['settings']["alternative_hosts"]
         if zoomUser != "":
             url = "https://api.zoom.us/v2/users/" + str(zoomUser)
             payload = {}
-            try :
-                response = requests.request("GET", url, headers=headers, data=payload)
-                data=json.loads(response.text)
-                if data["type"] != 2 :
-                    print("\n*** " + (zoomUser) + " (AltHost) is a free user. " + "Class_No " + eachMeeting["Saba_ID"])
+            try:
+                response = requests.request(
+                    "GET", url, headers=headers, data=payload)
+                data = json.loads(response.text)
+                if data["type"] != 2:
+                    print("\n*** " + (zoomUser) + " (AltHost) is a free user. "
+                          + "Class_No " + eachMeeting["Saba_ID"])
                     hostStatus = "False"
-                else :
-                    print('.', end='')
-            except  :
-                print("\n*** " + (zoomUser) + " (AltHost) is not in this account's Zoom user list. " + "Class_No " + eachMeeting["Saba_ID"])
+                else:
+                    print('.')
+            except:
+                print("\n*** " + (zoomUser) + " (AltHost) is not in this "
+                      + "account's Zoom user list. "
+                      + "Class_No " + eachMeeting["Saba_ID"])
                 hostStatus = "False"
         else:
-            print("\n--- Class_No " + eachMeeting["Saba_ID"] + " has no alternate host")
+            print("\n--- Class_No "
+                  + eachMeeting["Saba_ID"] + " has no alternate host")
 
     return(hostStatus)
 
 # Function to create new zoom meetings for every meeting read in from csv file
+
+
 def createMeetings(meetingsList):
     print("\nCreating Zoom Meetings")
     if bearer != "":
@@ -183,46 +294,52 @@ def createMeetings(meetingsList):
         user = eachMeeting['host']
         response = requests.post(
             f'https://api.zoom.us/v2/users/{user}/meetings',
-                headers=headers,
-                data=json.dumps(eachMeeting))
+            headers=headers,
+            data=json.dumps(eachMeeting))
 
         response_json_object = json.loads(response.text)
         response_json_object["Saba_ID"] = eachMeeting["Saba_ID"]
         meetingsReport.append(
             {
-            "Saba_ID":response_json_object["Saba_ID"],
-            "Zoom_UUID":response_json_object["uuid"],
-            "Zoom_ID":response_json_object["id"],
-            "start_url":response_json_object["start_url"],
-            "join_url":response_json_object["join_url"]
+                "Saba_ID": response_json_object["Saba_ID"],
+                "Zoom_UUID": response_json_object["uuid"],
+                "Zoom_ID": response_json_object["id"],
+                "start_url": response_json_object["start_url"],
+                "join_url": response_json_object["join_url"]
             })
 
-        print("Created Zoom Meeting ID" + str(response_json_object["id"]) +
-              " with Saba_ID " + response_json_object["Saba_ID"])
+        print("Created Zoom Meeting ID" + str(response_json_object["id"])
+              + " with Saba_ID " + response_json_object["Saba_ID"])
     return(meetingsReport)
 
 # Create MeetingReport output to send back to Saba
+
+
 def saveMeetingsReportJSON(meetingsReport):
     if (outputFileJSON != ""):
-        try :
+        try:
             with open((outputFileJSON), 'w', encoding='utf-8') as f:
                 json.dump(meetingsReport, f, ensure_ascii=False, indent=2)
-            print("Output File " + str(outputFileJSON) + " with Saba-Zoom info created.")
-        except :
+            print("Output File " + str(outputFileJSON)
+                  + " with Saba-Zoom info created.")
+        except:
             print("*** Failed to write JSON output File. \n")
+
+
 def saveMeetingsReportCSV(meetingsReport):
     if (outputFileCSV != ""):
-        fields = ["Saba_ID","Zoom_UUID","Zoom_ID","start_url","join_url"]
-        try :
+        fields = ["Saba_ID", "Zoom_UUID", "Zoom_ID", "start_url", "join_url"]
+        try:
             with open((outputFileCSV), 'w', encoding='utf-8') as f:
-                #Define the header row
-                writer = csv.DictWriter(f,fieldnames = fields)
-                #Write the header row
+                # Define the header row
+                writer = csv.DictWriter(f, fieldnames=fields)
+                # Write the header row
                 writer.writeheader()
-                #Fill in data rows
+                # Fill in data rows
                 writer.writerows(meetingsReport)
-            print("Output File " + str(outputFileCSV) + " with Saba-Zoom info created.")
-        except :
+            print("Output File " + str(outputFileCSV)
+                  + " with Saba-Zoom info created.")
+        except:
             print("*** Failed to write CSV output File. \n")
 
 
@@ -234,13 +351,16 @@ def main():
     hostsLicensed = "False"
     hostsLicensed = checkLicense(meetingsList)
 
-    #If all the hosts are licensed we can proceed with creating Zoom Meetings and generating report
+    # If all the hosts are licensed we can proceed with creating Zoom Meetings
+    # and generating report
     if (hostsLicensed == "True"):
         meetingsReport = createMeetings(meetingsList)
         saveMeetingsReportJSON(meetingsReport)
         saveMeetingsReportCSV(meetingsReport)
     else:
-        print("\n*** Didn't create meetings report because not all hosts and alternates were licensed.")
+        print("\n*** Didn't create meetings report because not all "
+              + "hosts and alternates were licensed.")
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
